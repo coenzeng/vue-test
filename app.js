dc47d78a36b0002982c1f3c088aa724855901e5d
Coen Zeng <czeng@coveo.com>
2021-09-10 20:25:17 -0400

    create lesson-3

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const app = Vue.createApp({
     //data, functions
     data() {
         return {
+            url: "https://google.com",
             showBooks: true,
-            title: 'The FInal Empire',
-            author: 'Coen',
-            age: '18'
+            books: [
+                { title: 'book1', author: 'oda', isFav: true},
+                { title: 'book2', author: 'oda1',isFav: true},
+                { title: 'book3', author: 'oda2', isFav: true},
+                { title: 'book4', author: 'oda3', isFav: false}
+            ]
         }
     },
     methods: {
@@ -15,6 +19,17 @@ const app = Vue.createApp({
         },
         changeShowBooks(){
             this.showBooks = !this.showBooks 
+        },
+        handleEve(e){
+            console.log(e, e.type)
+        },
+        toggleFav(book){
+            book.isFav = !book.isFav
+        }
+    },
+    computed: { //computed data depends on other data, it will change when other data changes
+        filteredBooks(){ //filter books only that are favourite
+            return this.books.filter((book) => book.isFav)
         }
     }
 })
--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
 Web app created using Vue.
+
+Notes:
+
+- Data Attributes: shortform of `v-bind:href` is `:href`
+- outputting lists (v-for)
+- dynamic classes
+- computed properties (creating a list of only facourite books)
