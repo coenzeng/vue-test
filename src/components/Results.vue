7fb37abeb4b19954c7075a2a24c62838892ce71d
Coen Zeng <czeng@coveo.com>
2021-09-12 20:58:03 -0400

    create results component

---
+++ src/components/Results.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>Reaction time: {{ score }} ms </p>
+  <h3> {{ rank }} </h3>
+</template>
+
+<script>
+export default {
+    props: [ 'score' ],
+    data(){
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 300){
+            this.rank = "You're insanse Dude"
+        } else {
+            this.rank = "You're trash, gg ez"
+        }
+
+    
+    }
+}
+</script>
+
+<style>
+
+</style>
