e1f34284e9b96c7a466d4761abf67ad67aaeb83b
Coen Zeng <czeng@coveo.com>
2021-09-12 20:17:45 -0400

    start new reaction game

---
+++ src/components/Block.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class='block'>
+      click me
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['delay']
+}
+</script>
+
+<style>
+
+.block{
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: block;
+  font-size: 16px;
+}
+
+</style>
